031f0604d2cb9b83d3633aaf4728d2548e9e9239
Joel Fernandes <up201904977@fe.up.pt>
2023-03-30 09:47:36 +0100

    chore: small fixes and parse parameters

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
-#include "Utils\utils.h"
-#include "Lexer\lexer.h"
+#include "Utils/utils.h"
+#include "Lexer/lexer.h"
 
-int main()
+void printUsage(const char* ex) {
+    printf("Usage:\n%s [inputFile]\n", ex);
+}
+
+int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        printf("Wrong number of arguments!\n\n");
+        printUsage(argv[0]);
+        return -1;
+    }
+
     const char *input;
 
-    if ((input = readFile("input.txt")) == NULL)
+    if ((input = readFile(argv[1])) == NULL)
     {
         printf("Error reading source file\n");
         return -1;
